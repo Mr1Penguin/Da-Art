8700bb58e78e426814875f1143ee3d5264ef9c88
Igor Sharapov <igorsharapov@outlook.com>
2018-10-28 02:49:31 +0300

    Style and InfinityCollection class

--- Da-Art/InfinityCollection.h
+++ Da-Art/InfinityCollection.h
@@ -1,20 +1,48 @@
 #pragma once
 
-#include "Wrapper.h"
 #include "Models.DeviationListItem.h"
-#include <winrt/base.h>
+#include "Wrapper.h"
 #include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/base.h>
 
-namespace winrt::Da_Art::Models
-{
+namespace winrt::Da_Art::Models {
+  namespace wf = winrt::Windows::Foundation;
   namespace wfc = winrt::Windows::Foundation::Collections;
+  namespace wuc = winrt::Windows::UI::Core;
   namespace wuxd = winrt::Windows::UI::Xaml::Data;
 
-  template<class T>
-  struct InfinityCollection : 
-    winrt::implements<wfc::IObservableVector<T>, wfc::IVector<T>, wfc::IVectorView<T>, wfc::IIterable<T>, wuxd::ISupportIncrementalLoading>,
-    winrt::observable_vector_base<InfinityCollection<T>, T>
-  {
+  template <class T>
+  struct InfinityCollection : winrt::implements<wfc::IObservableVector<T>, wfc::IVector<T>, wfc::IVectorView<T>,
+                                                wfc::IIterable<T>, wuxd::ISupportIncrementalLoading>,
+                              winrt::observable_vector_base<InfinityCollection<T>, T> {
+    bool HasMoreItems() const noexcept { return m_has_more_items; }
+    wf::IAsyncAction ContinueLoadingAsync()
+    {
+      HasMoreItems(true);
+      LoadMoreItemsAsync(1);
+    }
+    wf::IAsyncOperation<wuxd::LoadMoreItemsResult> LoadMoreItemsAsync(uint32_t count)
+    {
+      auto core_dispatcher = winrt::Windows::UI::Core::CoreWindow::GetForCurrentThread().Dispatcher();
+      LoadItems(count, core_dispatcher);
+    }
 
+  protected:
+    void HasMoreItems(bool has_more_items) noexcept { m_has_more_items = has_more_items; }
+    virtual wf::IAsyncOperation<wuxd::LoadMoreItemsResult> LoadItems(uint32_t count, wuc::CoreDispatcher) = 0;
+    virtual wf::IAsyncOperation<T> AddloadingItem() = 0;
+    void RemoveLoadingItem(T item, bool isError = false, winrt::hstring errorText = L"")
+    {
+      uint32_t index;
+      if (IndexOf(item, index)) { RemoveAt(index); }
+      if (!isError) { return; }
+
+      //TODO: propagate error
+      //ErrorOnLoading?.Invoke(this, null);
+      //Send event using global message queue
+    }
+
+  private:
+    bool m_has_more_items;
   };
-}
+} // namespace winrt::Da_Art::Models
--- Da-Art/Wrapper.h
+++ Da-Art/Wrapper.h
@@ -2,19 +2,19 @@
 
 #include <memory>
 
-namespace winrt::Da_Art::Models
-{
+namespace winrt::Da_Art::Models {
 
-  template<class T>
-  struct Wrapper
-  {
+  template <class T>
+  struct Wrapper {
     T Item() const noexcept { return m_item; }
     void Item(T item) noexcept { m_item = std::move(item); }
+
   protected:
     Wrapper() {}
-    Wrapper(T item) : m_item(std::move(item)) { }
+    Wrapper(T item) : m_item(std::move(item)) {}
     virtual ~Wrapper() = 0;
+
   private:
     T m_item;
   };
-}
+} // namespace winrt::Da_Art::Models
