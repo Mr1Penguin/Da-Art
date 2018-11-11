97e7bf6ea71b22e9c173c94a1bc93f90082f6e09
Igor Sharapov <igorsharapov@outlook.com>
2018-11-12 01:13:45 +0300

    add Pageable model

---
+++ UwpDeviation/Models.Pageable.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+
+#include "Models.Pageable.h"
+
+using namespace winrt;
+
+namespace winrt::UwpDeviation::Models::implementation {
+  void Pageable::Serialize([[maybe_unused]] rwriter &writer)
+  {
+    BaseModel::Serialize(writer);
+    Write(m_cursor, writer, L"cursor");
+    Write(m_has_more, writer, L"has_more");
+  }
+
+  void Pageable::Deserialize([[maybe_unused]] const rvalue &document) {
+    BaseModel::Deserialize(document);
+    m_cursor = Read<decltype(m_cursor)>(document, L"cursor");
+    m_has_more = Read<decltype(m_has_more)>(document, L"has_more");
+  }
+} // namespace winrt::UwpDeviation::Models::implementation
---
+++ UwpDeviation/Models.Pageable.h
@@ -0,0 +1,31 @@
+﻿//
+// Declaration of the Pageable class.
+//
+
+#pragma once
+
+#include "Models.BaseModel.h"
+#include "Models.Pageable.g.h"
+
+namespace winrt::UwpDeviation::Models::implementation {
+  struct Pageable : PageableT<Pageable, BaseModel> {
+    Pageable() = default;
+
+    winrt::hstring Cursor() const noexcept { return m_cursor; }
+    void Cursor(const winrt::hstring& cursor) noexcept { m_cursor = cursor; }
+    bool HasMore() const noexcept { return m_has_more; }
+    void HasMore(bool has_more) noexcept { m_has_more = has_more; }
+
+    virtual void Serialize([[maybe_unused]] rwriter &writer) override;
+    virtual void Deserialize([[maybe_unused]] const rvalue &document) override;
+    
+  private:
+    winrt::hstring m_cursor;
+    bool m_has_more;
+  };
+} // namespace winrt::UwpDeviation::Models::implementation
+
+namespace winrt::UwpDeviation::Models::factory_implementation {
+  struct Pageable : PageableT<Pageable, implementation::Pageable> {
+  };
+} // namespace winrt::UwpDeviation::Models::factory_implementation
